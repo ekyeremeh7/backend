e973a59fcccd0b4f60b04bc43aab8a1e9cf25ca7
ekyeremeh7 <ekyeremeh7@gmail.com>
2021-03-31 02:17:10 +0000

    reset connection file

--- mysql_connection/connections.js
+++ mysql_connection/connections.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
   host: "us-cdbr-east-03.cleardb.com",
   user: "bb3e722db94689",
   password: "dcae8df9",
@@ -8,55 +8,18 @@ const mysqlConnection = mysql.createConnection({
   multipleStatements: true,
 });
 
-// const mysqlConnection = mysql.createPool({
-//   host: "127.0.0.1",
-//   user: "root",
-//   password: "",
-//   database: "tekcare",
-//   multipleStatements: true,
-// });
-
-mysqlConnection.connect(function(err){
-  if(err) throw err;
-  console.log("Connected !!!");
-  let createUsers = `CREATE TABLE IF NOT EXISTS users(
-    id int primary key auto_increment,
-    name varchar(250) not null,
-    email varchar(250) not null,
-    password varchar(250) not null,
-    email_verified int default 0,
-    verification_code varchar(10)
-)`;
-
-mysqlConnection.query(createUsers,function(err,results,fields){
-  if(err) throw err;
-  console.log("Table Created !!!");
+mysqlConnection.on("connection", function (connection) {
+  console.log("... connected");
 });
 
-mysqlConnection.end(function(err){
-  if(err){
-    return console.log(err.message);
-  }
+mysqlConnection.on("acquire", function (connection) {
+  console.log("Connection %d acquired", connection.threadId);
 });
-
-});
-
-// mysqlConnection.on("connection", function (connection) {
-//   console.log("... connected");
-// });
-
-// mysqlConnection.on("acquire", function (connection) {
-//   console.log("Connection %d acquired", connection.threadId);
-// });
 
 module.exports = mysqlConnection;
 
 
-
-
-// const mysql = require("mysql");
-
-// const mysqlConnection = mysql.createPool({
+// const mysqlConnection = mysql.createConnection({
 //   host: "us-cdbr-east-03.cleardb.com",
 //   user: "bb3e722db94689",
 //   password: "dcae8df9",
@@ -64,12 +27,38 @@ module.exports = mysqlConnection;
 //   multipleStatements: true,
 // });
 
-// mysqlConnection.on("connection", function (connection) {
-//   console.log("... connected");
+// // const mysqlConnection = mysql.createPool({
+// //   host: "127.0.0.1",
+// //   user: "root",
+// //   password: "",
+// //   database: "tekcare",
+// //   multipleStatements: true,
+// // });
+
+// mysqlConnection.connect(function(err){
+//   if(err) throw err;
+//   console.log("Connected !!!");
+//   let createUsers = `CREATE TABLE IF NOT EXISTS users(
+//     id int primary key auto_increment,
+//     name varchar(250) not null,
+//     email varchar(250) not null,
+//     password varchar(250) not null,
+//     email_verified int default 0,
+//     verification_code varchar(10)
+// )`;
+
+// mysqlConnection.query(createUsers,function(err,results,fields){
+//   if(err) throw err;
+//   console.log("Table Created !!!");
 // });
 
-// mysqlConnection.on("acquire", function (connection) {
-//   console.log("Connection %d acquired", connection.threadId);
+// mysqlConnection.end(function(err){
+//   if(err){
+//     return console.log(err.message);
+//   }
 // });
+
+// });
+
 
 // module.exports = mysqlConnection;
